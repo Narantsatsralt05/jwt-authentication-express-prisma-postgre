84a8e76ad1646cb5789e3c85addd6010972958da
Narantsatsralt05 <n.bumnasan@gmail.com>
2025-02-18 16:37:22 +0800

    change middleware

--- src/verify.ts
+++ src/verify.ts
@@ -2,21 +2,20 @@ import jwt from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
 
 export const verify = (req: Request, res: Response, next: NextFunction) => {
-  const { accessToken, refreshToken } = req.cookies;
-  if (!accessToken && !refreshToken) {
-    res.status(401).send("Access Denied. No token provided.");
+
+  let token = req.headers.authorization;
+
+  if (!token) {
+    res.status(401).send("Access Denied. No refresh token provided.");
     return;
   }
-
+  
+  const accessToken = token.toString().split(' ')[1]
 
   try {
     jwt.verify(accessToken!, process.env.ACCESS_TOKEN_SECRET!);
     next();
   } catch (error) {
-    if (!refreshToken) {
-      res.status(401).send("Access Denied. No refresh token provided.");
-      return;
-    }
     res.status(400).send("Invalid Token.");
   }
 };
